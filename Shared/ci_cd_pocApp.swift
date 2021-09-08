bd1416113ead3bf65d47d543b8ce11d751d1b186
Rudigus <rodrigo.matos9@hotmail.com>
2021-09-08 09:36:08 -0300

    initial xcode project files

---
+++ Shared/ci_cd_pocApp.swift
@@ -0,0 +1,17 @@
+//
+//  ci_cd_pocApp.swift
+//  Shared
+//
+//  Created by Rodrigo Matos Aguiar on 08/09/21.
+//
+
+import SwiftUI
+
+@main
+struct ci_cd_pocApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
